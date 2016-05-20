fb0c4a12370b842338defc902d17c5e6cefa90e1
willriggins <williamriggins2013@gmail.com>
2016-05-19 23:36:03 -0400

    Working Version for Assignment 4

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -2,18 +2,50 @@ package com.theironyard;
 
 import java.util.Scanner;
 
+import static com.theironyard.User.record; // had to import this to get record to work in the nextLine method below?
+
 public class Main {
 
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Welcome back!");
-        System.out.println("What is your name?");
 
         User user = new User();
 
-        user.chooseName();
-        user.chooseAction();
-        user.finishTransaction();
+
+        while (user.cycle) {
+            user.chooseName();
+            user.chooseAction();
+        }
     }
+
+    public static String nextLine() {
+        String line = scanner.nextLine();
+        while (line.startsWith("/")) {
+            if (line.equals("/hack")) {
+                System.out.println("Access Granted");
+                System.out.println();
+                if (record.size() == 0) {
+                    System.out.println("No existing records.");
+                }
+                else if (record.size() == 1) {
+                    System.out.println("1 existing record:");
+                    System.out.println(record);
+                }
+                else if (record.size() > 1) {
+                    System.out.println(record.size() + " existing records:");
+                    System.out.println(record); // why doesn't user.record work here w/o import?
+                }
+                System.out.println();
+                System.out.println("Please continue.");
+            }
+            else {
+                System.out.println("Error: Please repeat last input.");
+            }
+
+            line = scanner.nextLine();
+        }
+        return line;
+    }
+
 }
--- src/com/theironyard/User.java
+++ src/com/theironyard/User.java
@@ -1,68 +1,106 @@
 package com.theironyard;
 
+import java.util.HashMap;
+
 /**
  * Created by will on 5/18/16.
  */
 public class User {
-    String name;
+    static String name;
     String menuChoice;
     String num;
     int numInt;
+    double balance;
+    String response;
+    boolean inUse;
+    boolean cycle = true;
+    static HashMap<String, Double> record = new HashMap();
 
-    public void chooseName() throws Exception {
-        name = Main.scanner.nextLine();
 
-        if (name.isEmpty()) {
-            throw new Exception("Unable to locate your account.");
+    public void chooseName() {
+        System.out.println();
+        System.out.println("Welcome to the ATM. What is your name?");
+
+        name = Main.nextLine();
+
+        if (record.containsKey(name)) {
+            System.out.println("Welcome back " + name + ".");
+            inUse = true;
         }
         else {
-            System.out.println("Thanks for banking with us, " + name + ".");
+            System.out.println("We could not find an account under this name.");
+            System.out.println("Would you like to set up an account? Please choose an option:");
+            System.out.println("1 - Yes");
+            System.out.println("2 - No");
+            System.out.println();
+
+            response = Main.nextLine();
+
+            if (response.equalsIgnoreCase("Yes") || (response.equals("1"))) {
+                System.out.println("Thanks, " + name + ". Your account has been created.");
+                record.put(name, 0.0);
+                inUse = true;
+            }
+            else {
+                inUse = false;
+                System.out.println("Thank you. Please come again.");
+                //throw new Exception("Thank you. Please return if you wish to set up an account.");
+            }
         }
     }
 
     public void chooseAction() throws Exception {
-        System.out.println("What would you like to do? You may choose between the following options:");
-        System.out.println("1. Check your balance");
-        System.out.println("2. Withdraw funds");
-        System.out.println("3. Cancel");
-        System.out.println();
-        System.out.println("Enter the correct option number to continue.");
-    }
 
-    public void finishTransaction() throws Exception {
-        menuChoice = Main.scanner.nextLine();
+       //inUse = true; //working code
+        while (inUse) {
+            System.out.println();
+            System.out.println("What would you like to do? You may choose between the following options:");
+            System.out.println("1. Check your balance");
+            System.out.println("2. Withdraw funds");
+            System.out.println("3. Cancel");
+            System.out.println("4. Close your account.");
+            System.out.println();
+            System.out.println("Enter the correct option number to continue.");
 
-        if (menuChoice.isEmpty()) {
-            throw new Exception("You broke the bank.");
-        }
-        else if ((menuChoice.equals("1")) || menuChoice.equalsIgnoreCase("one")) {
-            System.out.println("Your balance is $100.");
-        }
-        else if ((menuChoice.equals("2")) || menuChoice.equalsIgnoreCase("two")) {
-            System.out.println("How much money would you like to withdraw?");
 
-            num = Main.scanner.nextLine();
+            menuChoice = Main.nextLine();
 
-            if (num.isEmpty()) {
-                throw new Exception("You must enter the amount that you wish to withdraw.");
+            if (menuChoice.isEmpty()) {
+                throw new Exception("You broke the bank.");
+            } else if ((menuChoice.equals("1")) || menuChoice.equalsIgnoreCase("one")) {
+                System.out.println("Your balance is $100.");
+            } else if ((menuChoice.equals("2")) || menuChoice.equalsIgnoreCase("two")) {
+                System.out.println("How much money would you like to withdraw?");
+
+                num = Main.nextLine();
+
+                if (num.isEmpty()) {
+                    throw new Exception("You must enter the amount that you wish to withdraw.");
+                } else {
+                    numInt = Integer.valueOf(num);
+
+                    if (numInt > 100) {
+                        throw new Exception("Insufficient funds available. You may only withdraw up to $100.");
+                    } else if (numInt < 1) {
+                        throw new Exception("You must withdraw at least $1.");
+                    } else {
+                        System.out.println("Please take your $" + num + ".");
+                    }
+                }
+            } else if ((menuChoice.equals("3")) || menuChoice.equalsIgnoreCase("three")) {
+                System.out.println("Thank you and please come again.");
+                inUse = false;
+            }
+            else if ((menuChoice.equals("4")) || menuChoice.equalsIgnoreCase("four")) {
+                System.out.println("Your account has been closed.");
+                System.out.println("Please consider opening a new account with us. Thank you.");
+                record.remove(name);
+                inUse = false;
             }
             else {
-                numInt = Integer.valueOf(num);
-
-                if (numInt > 100) {
-                    throw new Exception("Insufficient funds available. You may only withdraw up to $100.");
-                } else if (numInt < 1) {
-                    throw new Exception("You must withdraw at least $1.");
-                } else {
-                    System.out.println("Please take your $" + num + ".");
-                }
+                throw new Exception("You did not select a valid option.");
             }
-        }
-        else if ((menuChoice.equals("3")) || menuChoice.equalsIgnoreCase("three")) {
-            System.out.println("Thank you and please come again.");
-        }
-        else {
-            throw new Exception("You did not select a valid option.");
         }
     }
 }
+
